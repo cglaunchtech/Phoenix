5f7efaefdb73087d7e32580feb626bc12466361d
Devon Cowherd <devoncowherd@gmail.com>
2022-06-23 08:39:13 -0700

    meta data updated for key

--- app/src/main/java/com/example/sportssocial/MainActivity.kt
+++ app/src/main/java/com/example/sportssocial/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sportssocial
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ai: ApplicationInfo = applicationContext.packageManager
+            .getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
+        val newsKey = ai.metaData["newsKey"]
+
     }
 }
