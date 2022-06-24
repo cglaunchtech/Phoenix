7f32189249b75cbaba15a1bc066d7dda2afe8c94
jamhec <jamhector@gmail.com>
2022-06-23 23:08:58 -0400

    fixed binding

--- app/src/main/java/com/example/sportssociallayouts/ui/navigation/BottomNavMain.kt
+++ app/src/main/java/com/example/sportssociallayouts/ui/navigation/BottomNavMain.kt
@@ -6,22 +6,21 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.sportssociallayouts.R
-import com.example.sportssociallayouts.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_bottom_nav_main.view.*
+import com.example.sportssociallayouts.databinding.ActivityBottomNavMainBinding
 
 
 class BottomNavMain : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding:ActivityBottomNavMainBinding
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityBottomNavMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
-        setupWithNavController(binding.root.bottomNavigationView,navController)
+        setupWithNavController(binding.bottomNavigationView,navController)
     }
 }
