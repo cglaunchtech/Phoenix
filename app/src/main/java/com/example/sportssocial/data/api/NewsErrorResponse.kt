115e073dde137e9d5434943a72d3b50ba3a00c7c
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-07-06 11:43:00 -0700

    add Sandwich implementation

---
+++ app/src/main/java/com/example/sportssocial/data/api/NewsErrorResponse.kt
@@ -0,0 +1,10 @@
+package com.example.sportssocial.util
+
+/**
+ * Developed by skydoves on 2020-12-30.
+ * Copyright (c) 2018 skydoves rights reserved.
+ */
+data class NewsErrorResponse(
+    val code: Int,
+    val message: String
+)
--- app/src/main/java/com/example/sportssocial/data/api/RetrofitClient.kt
+++ app/src/main/java/com/example/sportssocial/data/api/RetrofitClient.kt
@@ -2,6 +2,7 @@ package com.example.sportssocial.data.api
 
 import com.example.sportssocial.BuildConfig
 import com.example.sportssocial.data.api.pojo.TopHeadlinesPojo
+import com.skydoves.sandwich.coroutines.CoroutinesResponseCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.reactivex.rxjava3.core.Observable
@@ -33,7 +34,9 @@ interface RetrofitClient {
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+//                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .addCallAdapterFactory(CoroutinesResponseCallAdapterFactory.create())
+//                .addCallAdapterFactory(ApiResponseCallAdapterFactory.create())
                 .build()
 
             return retrofit.create(RetrofitClient::class.java)
